9a6f25dd10267bca31b6f39275d98d47f2b98874
mcsoja <sojam@bc.edu>
2020-10-30 17:32:29 -0400

    DONE

--- script.js
+++ script.js
@@ -25,8 +25,6 @@ let svg = d3.select(".chart").append("svg")
 .attr("height", height + margin.top + margin.bottom)
 .append("g")
 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-
     
 const features = topojson.feature(data[1], data[1].objects.countries).features;
 const projection = d3.geoMercator()
@@ -61,112 +59,296 @@ let circleScale =
   .scaleLinear()
   .domain(d3.extent(passengersList))
   .range([4,9])
+  
 
-  let force = d3.forceSimulation(data[0].nodes)
-  .force("charge", d3.forceManyBody().strength(-25))
-  .force("link", d3.forceLink(data[0].links).distance(50))
-  .force("center",d3.forceCenter()
-      .x(width / 2)
-      .y(height / 2)
-  )
+    let force = d3.forceSimulation(data[0].nodes)
+    .force("charge", d3.forceManyBody().strength(-25))
+    .force("link", d3.forceLink(data[0].links).distance(50))
+    .force("center",d3.forceCenter()
+        .x(width / 2)
+        .y(height / 2)
+    )
 
-  drag = force => {
+    visType = "force";
+      switchLayout()
 
-    function dragstarted(event) {
-      if (!event.active) force.alphaTarget(0.3).restart();
-      event.subject.fx = event.subject.x;
-      event.subject.fy = event.subject.y;
-    }
-    
-    function dragged(event) {
-      event.subject.fx = event.x;
-      event.subject.fy = event.y;
-    }
-    
-    function dragended(event) {
-      if (!event.active) force.alphaTarget(0);
-      event.subject.fx = null;
-      event.subject.fy = null;
-    }
-    
-    return d3.drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended);
-  }
+    function switchLayout() {
+      console.log("SWITHC")
 
-let links = svg.selectAll('.chart')
+
+      if (visType === "map") {
+
+
+        //transition force nodes
+        linksForce = svg.selectAll('.chart')
         .data(data[0].links)
         .enter()
         .append('line')
+        .attr('class', 'force')
+        .attr('x1', (d)=> (d.source.x))
+        .attr('y1',(d) => (d.source.y))
+        .attr('x2', (d) => (d.target.x))
+        .attr('y2',(d) => (d.target.y))
+        .transition()
+        .duration(1000)
+        .attr("x1", function(d) {
+          return projection([d.source.longitude, d.source.latitude])[0];
+        })
+        .attr("y1", function(d) {
+          return projection([d.source.longitude, d.source.latitude])[1];
+        })
+        .attr("x2", function(d) {
+          return projection([d.target.longitude, d.target.latitude])[0];
+        })
+        .attr("y2", function(d) {
+          return projection([d.target.longitude, d.target.latitude])[1];
+        })
+        .attr('stroke', 'black')
+        .attr('stroke', 'black')
+
+        let linksMap = svg.selectAll('.chart')
+        .data(data[0].links)
+        .enter()
+        .append('line')
+        .attr('class', 'map')
+        .attr('x1', (d)=> (d.source.x))
+        .attr('y1',(d) => (d.source.y))
+        .attr('x2', (d) => (d.target.x))
+        .attr('y2',(d) => (d.target.y))
+        .transition()
+        .duration(1000)
+        .attr("x1", function(d) {
+          return projection([d.source.longitude, d.source.latitude])[0];
+        })
+        .attr("y1", function(d) {
+          return projection([d.source.longitude, d.source.latitude])[1];
+        })
+        .attr("x2", function(d) {
+          return projection([d.target.longitude, d.target.latitude])[0];
+        })
+        .attr("y2", function(d) {
+          return projection([d.target.longitude, d.target.latitude])[1];
+        })
+        .attr('stroke', 'black')
+
+
+        let nodesMap = svg.selectAll('.chart')
+        .data(data[0].nodes)
+        .enter()
+        .append('circle')
+        .attr('class', 'map')
+        .attr('cx', (d,i)=>(d.x))
+        .attr('cy', (d,i)=>(d.y))
+        .attr('fill', 'orange') 
+        .attr('r',d=>circleScale(d.passengers))
+        .on("mouseenter", (event, d) => {
+              const pos = d3.pointer(event, window)
+              d3.selectAll('.tooltip')
+                  .style('display','inline-block')
+                  .style('position','fixed')
+                  .style('top', pos[1]+'px')
+                  .style('left', pos[0]+'px')
+                  .html(
+                      d.name 
+                  )
+          })
+          .on("mouseleave", (event, d) => {
+              d3.selectAll('.tooltip')
+                  .style('display','none')
+              //console.log("HERE")
+          })
+        .transition()
+        .duration(1000)
+        .attr("cx", function(d) {
+          return projection([d.longitude, d.latitude])[0];
+        })
+        .attr("cy", function(d) {
+          return projection([d.longitude, d.latitude])[1];
+        })
+      //   .on("mouseenter", (event, d) => {
+      //     const pos = d3.pointer(event, window)
+      //     d3.selectAll('.tooltip')
+      //         .style('display','inline-block')
+      //         .style('position','fixed')
+      //         .style('top', pos[1]+'px')
+      //         .style('left', pos[0]+'px')
+      //         .html(
+      //             d.name 
+      //         )
+      // })
+      // .on("mouseleave", (event, d) => {
+      //     d3.selectAll('.tooltip')
+      //         .style('display','none')
+      //     //console.log("HERE")
+      // });
+
+        
+
+      // nodesForce = svg.selectAll('.chart')
+      //   .data(data[0].nodes)
+      //   .enter()
+      //   .append('circle')
+      //   .attr('class', 'force')
+      //   .attr('cx', (d,i)=>(d.x))
+      //   .attr('cy', (d,i)=>(d.y))
+      //   .attr('fill', 'orange') 
+      //   .attr('r',d=>circleScale(d.passengers))
+
+        svg.selectAll("path")
+              .attr("opacity", 0);
+
+
+        svg.selectAll('.force').remove()
+
+        force.alpha(0.5).stop();
+        //let links = data[0].links
+        
+
+   
+
+
+        // nodesMap = svg.selectAll('.chart')
+        // .transition()
+        // .duration(1000)
+        // .attr("transform", function(d){
+        //   return "translate(" + projection([d.longitude, d.latitude]) + ")";
+        // })
+      //   .on("mouseenter", (event, d) => {
+      //     const pos = d3.pointer(event, window)
+      //     d3.selectAll('.tooltip')
+      //         .style('display','inline-block')
+      //         .style('position','fixed')
+      //         .style('top', pos[1]+'px')
+      //         .style('left', pos[0]+'px')
+      //         .html(
+      //             d.name 
+      //         )
+      // })
+      // .on("mouseleave", (event, d) => {
+      //     d3.selectAll('.tooltip')
+      //         .style('display','none')
+      //     //console.log("HERE")
+      // });
+        
+        force.on("tick", () => {
+          linksMap
+          .attr("x1", function(d) {
+            return projection([d.source.longitude, d.source.latitude])[0];
+          })
+          .attr("y1", function(d) {
+            return projection([d.source.longitude, d.source.latitude])[1];
+          })
+          .attr("x2", function(d) {
+            return projection([d.target.longitude, d.target.latitude])[0];
+          })
+          .attr("y2", function(d) {
+            return projection([d.target.longitude, d.target.latitude])[1];
+          });
+        
+          nodesMap
+          .attr("transform", function(d){
+            return "translate(" + projection([d.longitude, d.latitude]) + ")";
+          })
+
+          drag.filter(event => visType === "force")
+        
+        });
+
+        //nodesMap.append("title").text(d=>d.name);
+        
+        
+        svg.selectAll('path')
+        .transition()
+              .delay(500)
+        .attr('opacity', 1)
+
+          // nodes.attr("cx", function(d) {
+          //   return projection([d.longitude, d.latitude])[0];
+          // })
+
+      } else { // force layout
+        //move Force nodes and links
+
+        
+
+
+
+
+
+        svg.selectAll('.map').remove()
+
+        drag = force => {
+
+          function dragstarted(event) {
+            if (!event.active) force.alphaTarget(0.3).restart();
+            event.subject.fx = event.subject.x;
+            event.subject.fy = event.subject.y;
+          }
+          
+          function dragged(event) {
+            event.subject.fx = event.x;
+            event.subject.fy = event.y;
+          }
+          
+          function dragended(event) {
+            if (!event.active) force.alphaTarget(0);
+            event.subject.fx = null;
+            event.subject.fy = null;
+          }
+          
+          return d3.drag()
+              .on("start", dragstarted)
+              .on("drag", dragged)
+              .on("end", dragended);
+        }
+        force.alpha(0.5).restart();
+        // set the map opacity to 0
+
+        let linksForce = svg.selectAll('.chart')
+        .data(data[0].links)
+        .enter()
+        .append('line')
+        .attr('class', 'force')
         .attr('x1', (d)=> (d.source.x))
         .attr('y1',(d) => (d.source.y))
         .attr('x2', (d) => (d.target.x))
         .attr('y2',(d) => (d.target.y))
         .attr('stroke', 'black')
 
-let nodes = svg.selectAll('.chart')
+      let nodesForce = svg.selectAll('.chart')
         .data(data[0].nodes)
         .enter()
         .append('circle')
+        .attr('class', 'force')
         .attr('cx', (d,i)=>(d.x))
         .attr('cy', (d,i)=>(d.y))
         .attr('fill', 'orange') 
         .attr('r',d=>circleScale(d.passengers))
         .call(drag(force));
+        svg.selectAll("path")
+              .attr("opacity", 0);
 
-force.on("tick", () => {
-  links
-      .attr("x1", d => (d.source.x))
-      .attr("y1", d => (d.source.y))
-      .attr("x2", d => (d.target.x))
-      .attr("y2", d => (d.target.y));
+        force.on("tick", () => {
+        linksForce
+            .attr("x1", d => (d.source.x))
+            .attr("y1", d => (d.source.y))
+            .attr("x2", d => (d.target.x))
+            .attr("y2", d => (d.target.y));
+      
+        nodesForce
+            .attr("cx", d => (d.x))
+            .attr("cy", d => (d.y))
+      
+      });
+      }
+      
+    }
 
-  nodes
-      .attr("cx", d => (d.x))
-      .attr("cy", d => (d.y))
-
-});
-
-
-
-nodes.append("title")
-    .text(d=>d.name);
-    
     d3.selectAll("input[name=display]").on("change", event=>{
       visType = event.target.value;// selected button
       console.log("SWITHC")
       switchLayout();
     });
-
-    function switchLayout() {
-      console.log("SWITHC")
-      if (visType === "map") {
-        // stop the simulation
-        force.stop()
-        // set the positions of links and nodes based on geo-coordinates
-        //let nodes = data[0].nodes
-        //let links = data[0].links
-        
-        
-        svg.selectAll('path')
-        .attr('opacity', 1)
-
-          nodes.attr("cx", function(d) {
-            return projection([d.longitude, d.latitude])[0];
-          })
-          drag.filter(event => visType === "force")
-
-      } else { // force layout
-
-        force.alpha(0.5).restart();
-        // set the map opacity to 0
-        svg.selectAll("path")
-              .attr("opacity", 0);
-          // set the map opacity to 0
-      }
-      
-    }
     
 
 
